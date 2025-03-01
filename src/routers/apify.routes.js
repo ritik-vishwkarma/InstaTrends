38bc1bc2f564dff0f363e5a78bddb572c3f22786
ritik-vishwkarma <vishwakarmaritik1563@gmail.com>
2025-03-01 23:42:05 +0530

    Refactor apify.routes.js to clean up commented code and improve readability

--- src/routers/apify.routes.js
+++ src/routers/apify.routes.js
@@ -1,13 +1,20 @@
 import { Router } from "express";
 import { runActor } from "../controllers/apify.controller.js";
-// import { storeCollectionName } from "../middleware/store.collectionName.js";
-// import { answerQuery } from "../llm/index.js";
 import { queryEndpoint } from "../llm/llm_query.js";
 
 const router = Router();
 
 router.route("/run-actor").post(runActor);
-// router.route("/query").post(answerQuery)
 router.route("/llmquery").post(queryEndpoint)
 
 export default router;
+
+
+
+
+
+
+// For Middleware, if needed in the future
+// import { storeCollectionName } from "../middleware/store.collectionName.js";
+// import { answerQuery } from "../llm/index.js";
+// router.route("/query").post(answerQuery)
