ed749045ada0e39fe6693bec4d268ac196443b7f
RITIK VISHWAKARMA <115628392+ritik-vishwkarma@users.noreply.github.com>
2025-03-06 19:08:43 +0530

    Update apify.routes.js

--- src/routers/apify.routes.js
+++ src/routers/apify.routes.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 import { runActor } from "../controllers/apify.controller.js";
 // import { storeCollectionName } from "../middleware/store.collectionName.js";
 import { answerQuery } from "../llm/index.js";
-import { queryEndpoint } from "../llm/llm_query.js";
+// import { queryEndpoint } from "../llm/llm_query.js";
 
 const router = Router();
 
